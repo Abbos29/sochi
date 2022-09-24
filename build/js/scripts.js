51554642afee85b73e7e6f44fdb5b024f5f13a23
abbos <abbosvaliev2003@gmail.com>
2022-09-24 22:11:24 +0500

    feat: add tour(slider) section

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,26 @@
 // Custom Scripts
 // Custom scripts
 
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+
+    // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    720: {
+        slidesPerView: 3,
+        spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+        slidesPerView: 2,
+        spaceBetween: 20
+    },
+    310: {
+        slidesPerView: 1,
+        spaceBetween: 10
+    },
+    }
+});

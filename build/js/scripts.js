0b99ae264bc031f2bc37f11f113279a7228c5bd8
abbos <abbosvaliev2003@gmail.com>
2022-09-24 23:58:48 +0500

    feat: add reviews section

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -24,3 +24,28 @@ const swiper = new Swiper('.swiper', {
     },
     }
 });
+
+const swiperReviews = new Swiper('.reviews__swiper', {
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+
+    // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    720: {
+        slidesPerView: 1,
+        spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+    },
+    310: {
+        slidesPerView: 1,
+        spaceBetween: 10
+    },
+    }
+});
